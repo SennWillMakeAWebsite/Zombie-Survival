b729d1a1f6e9ccc898bfd4b698c7cb32798fc7df
SennWillMakeAWebsite <hussein.ulumudin01@gmail.com>
2026-07-31 15:37:41 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+const canvas =
+document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+//Fullscreen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//Player
+const player ={
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 40,
+    speed: 5,
+    color: "#00BFFF"
+};
+
+//Keyboard
+const keys = {};
+
+window.addEventListener("keydown", (e) => {
+    keys[e.key.toLowerCase()] =
+    false;
+});
+
+//Update
+function update(){
+
+    if(keys["w"]) player.y -=
+    player.speed;
+    if(keys["s"]) player.y +=
+    player.speed;
+    if(keys["a"]) player.x -=
+    player.speed;
+    if(keys["d"]) player.x +=
+    player.speed;
+}
+
+// Draw
+function draw() {
+    ctx.clearRect(0, 0, canvas.width,
+    canvas.height);
+
+        ctx.beginPath();
+        ctx.arc(player .x, player .y, 
+            player .radius, 0, Math.PI * 2);
+            ctx.fillStyle = player.color;
+            ctx.fill();
+}
+
+// Loop game
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
