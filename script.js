fc77e35d4e3095c122d3627c784f878a6fb41198
SennWillMakeAWebsite <hussein.ulumudin01@gmail.com>
2026-08-20 10:32:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// Background hitam
-ctx.fillStyle = "black";
+ctx.fillStyle = "#111";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-// Lingkaran merah
+ctx.fillStyle = "#00bfff";
 ctx.beginPath();
-ctx.arc(400, 300, 50, 0, Math.PI * 2);
-ctx.fillStyle = "red";
+ctx.arc(
+    canvas.width / 2,
+    canvas.height / 2,
+    40,
+    0,
+    Math.PI * 2
+);
 ctx.fill();
+
+ctx.fillStyle = "white";
+ctx.font = "30px Arial";
+ctx.textAlign = "center";
+ctx.fillText(
+    "ZOMBIE SURVIVAL",
+    canvas.width / 2,
+    canvas.height / 2 + 100
+);
+
+console.log("Zombie Survival JS berhasil dijalankan!");
